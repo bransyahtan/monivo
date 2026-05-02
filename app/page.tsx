371405e5ac0d622641f2e108f3802267a8f213a3
bransyah <bransyahhhh@gmail.com>
2026-05-03 00:57:21 +0700

    feat: implement initial landing page with global styles, assets, and loading screen component

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,23 @@
+"use client";
+import { useState, useEffect } from "react";
 import Image from "next/image";
+import Loading from "@/components/Loading";
 
 export default function Home() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    // Simulasi loading selama 2 detik
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+    }, 2000);
+    return () => clearTimeout(timer);
+  }, []);
+
+  if (isLoading) {
+    return <Loading />;
+  }
+
   return (
     <div>
       <main>
